c2f25220885035b5e4cf33466e47e2ba85b344a6
quentindeltour <quentindeltour@orange.fr>
2023-04-22 15:42:14 +0200

    Add python script reading file from lichess.org

---
+++ read_pgn_from_lichess_to_json.py
@@ -0,0 +1,41 @@
+import json
+import chess
+import io
+import chess.pgn
+from collections import defaultdict
+import urllib.request
+
+# URL of the pgn file
+url = "https://lichess.org/api/broadcast/lrdHUzyS.pgn"
+
+# Read the pgn file from the url
+pgn_file = urllib.request.urlopen(url)
+pgn_data = io.TextIOWrapper(pgn_file, encoding='utf-8', errors='ignore')
+
+dic = defaultdict(dict)
+for nb_game in range(1,15):
+    game = chess.pgn.read_game(pgn_data)
+    if game is None:
+        pass  # end of file
+    else:
+        event = game.headers.get("Event")
+        white = res =  ' '.join(game.headers.get("White").split(", ")[::-1])
+        black = res =  ' '.join(game.headers.get("Black").split(", ")[::-1])
+        round = "Ronde {}".format(game.headers.get("Round").split(".")[0])
+        winner = game.headers.get("Result")
+        print(round)
+        dic["round_{}".format(round)] = {
+            "event":event, 'round':round, "round2":nb_game, "white":white, "black":black, "resultat":winner, "moves":{}
+        }
+        for node in game.mainline():
+            dic["round_{}".format(round)]["moves"]["{}:{}".format(node.ply(), node.san())] =  {
+                "move_number":node.ply(), "move":node.san(),
+                "turn": node.turn(), 
+                # "eval" : node.eval(), 
+                # "clock":node.clock(),
+                "proba_win_pov_white":node.eval().wdl().white().expectation(),
+                "evaluation_pov_white":node.eval().white().score(mate_score=10000)
+            }
+
+with open("./data/data.json", "w") as f:
+    json.dump(dic, f, indent=4)
